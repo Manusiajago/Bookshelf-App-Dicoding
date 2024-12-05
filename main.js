fc45f3e5094b97839ae5b78e7e469d4f3c721932
egal <egalassegaf@gmail.com>
2024-12-05 19:04:46 +0700

    create UI & javascript logic

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 // Do your work here...
-console.log('Hello, world!');
+// console.log('Hello, world!');
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputBookTitle = document.getElementById('bookFormTitle');
+    const inputBookAuthor = document.getElementById('bookFormAuthor');
+    const inputBookYear = document.getElementById('bookFormYear');
+})
